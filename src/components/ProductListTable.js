3ee1e4e4d376ae9222e10e22f157cfd791eb15c4
LuchoAiello <luchoaiello1@gmail.com>
2022-01-09 20:40:35 -0300

    cambio en productListTable

--- src/components/ProductListTable.js
+++ src/components/ProductListTable.js
@@ -6,7 +6,7 @@ function ProductsListTable(props){
             <div className="col-lg-6 mb-4 cardP">
                 <div className=" text-white bg-secondary  shadow cardD">
                 <div className="card-body card-bodyList ">
-                    <a className='cardE' href={props.detail}>{props.title}</a>
+                    <a target="_blank" className='cardE' href={props.detail}>{props.title}</a>
                     </div>
                 </div>
             </div>
